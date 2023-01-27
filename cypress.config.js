40ea22826ae34dd9973281aaf0f6dc0c613b10a2
qqyurr <byyulli16@gmail.com>
2023-01-27 14:17:21 +0900

    test2

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
+module.exports = {
+  // The rest of the Cypress config options go here...
+  projectId: "b3m8t7",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+};
