70ddca34e404dad7af605b47a50e91bf67fc6128
qqyurr <byyulli16@gmail.com>
2023-01-27 15:12:12 +0900

    testest

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // The rest of the Cypress config options go here...
   projectId: "b3m8t7",
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
