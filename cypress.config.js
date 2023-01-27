992b59560bc5faac321832f4ac63b3a13f7e1dd7
qqyurr <byyulli16@gmail.com>
2023-01-27 15:23:03 +0900

    config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // The rest of the Cypress config options go here...
   projectId: "b3m8t7",
+  screenshotOnRunFailure: false,
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
