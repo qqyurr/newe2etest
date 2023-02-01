9e3e5e80c66ee8486f72ea1ad22ae7502d1100a0
qqyurr <byyulli16@gmail.com>
2023-02-01 15:07:37 +0900

    fail case

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // The rest of the Cypress config options go here...
   projectId: "b3m8t7",
-  screenshotOnRunFailure: false,
-  video: false,
+  screenshotOnRunFailure: true,
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('Does not do much!', () => {
+    expect(false).to.equal(true)
   })
 })
