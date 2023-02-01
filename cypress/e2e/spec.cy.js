c9d8fed11663d7ef30aa83598ced59e8cd53a991
qqyurr <byyulli16@gmail.com>
2023-02-01 15:16:59 +0900

    fail success case

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('Does not do much!', () => {
-    expect(false).to.equal(true)
-  })
+describe('first test', { testIsolation: false }, () => {
+  it('Render', () => {
+    cy.visit('/');
+  });
+
+  it ('entered to do 1', () => {
+    cy.get('[data-cy="test-link"]')
+      .should('have.value','Learn React 2')
+  });
+
+  it ('entered to do 2', () => {
+    cy.get('[data-cy="test-link"]')
+      .should('have.value','Learn React 1')
+  });
 })
